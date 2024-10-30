c705891d22c379aeb0ca3f435784445cedfadb24
Andres Mendez <fmendez1212@gmail.com>
2024-10-30 05:16:52 -0500

    format document

--- dungeon1/src/core/core.cpp
+++ dungeon1/src/core/core.cpp
@@ -2,7 +2,9 @@
 #include "loadlibrary.h"
 #include <engine.h>
 #include <externals.h>
+
 #include <game.h>
+
 #include <stdio.h>
 
 #include <Windows.h>
@@ -24,11 +26,14 @@
 
 std::atomic<bool> reloadFlag(false);
 
-void waitforreloadsignal(HANDLE hEvent) {
+void waitforreloadsignal(HANDLE hEvent)
+{
   std::cout << "Current wating for reload signal" << std::endl;
-  while (true) {
+  while (true)
+  {
     DWORD dwWaitResult = WaitForSingleObject(hEvent, INFINITE);
-    if (dwWaitResult == WAIT_OBJECT_0) {
+    if (dwWaitResult == WAIT_OBJECT_0)
+    {
       print_log("Hot reload signal received", COLOR_GREEN);
       reloadFlag.store(true);
       break;
@@ -36,21 +41,23 @@ void waitforreloadsignal(HANDLE hEvent) {
   }
 }
 
-
-std::string getCurrentWorkingDirectory() {
+std::string getCurrentWorkingDirectory()
+{
   char buffer[MAX_PATH];
   GetCurrentDirectoryA(MAX_PATH, buffer);
   return std::string(buffer);
 }
 
-EXPORT void init() {
+EXPORT void init()
+{
   printf("Core initialized\n");
 
   std::string cwd = getCurrentWorkingDirectory();
   std::cout << "Current working directory: " << cwd << std::endl;
 
   HANDLE hEvent = CreateEventA(NULL, TRUE, FALSE, HOTRELOAD_EVENT_NAME);
-  if (hEvent == NULL) {
+  if (hEvent == NULL)
+  {
     std::cerr << "CreateEvent failed (" << GetLastError() << ")" << std::endl;
   }
   std::thread signalThread(waitforreloadsignal, hEvent);
@@ -58,8 +65,9 @@ EXPORT void init() {
 
   game g;
   g.world = malloc(100 * 1024);
-  if (g.world != NULL) {
-    memset(g.world, 0, 100 * 1024);  // Set all allocated memory to zero
+  if (g.world != NULL)
+  {
+    memset(g.world, 0, 100 * 1024); // Set all allocated memory to zero
   }
 
   std::string src = cwd + "\\build\\debug\\engine.dll";
@@ -70,7 +78,7 @@ EXPORT void init() {
 
   init_engine_func init_engine = (init_engine_func)getfunction(g.engine_lib, "init_engine");
   init_engine(&g);
-  load_level_func  load_level = (load_level_func)getfunction(g.engine_lib, "load_level");
+  load_level_func load_level = (load_level_func)getfunction(g.engine_lib, "load_level");
   load_level(&g, "scene.toml");
 
   assign_hotreloadable((hotreloadable_imgui_draw_func)getfunction(
@@ -79,12 +87,12 @@ EXPORT void init() {
   init_externals(&g);
 
   begin_watch_src_directory(g);
-  
+
   begin_game_loop(g);
 }
 
-
-void compile_dll() {
+void compile_dll()
+{
   std::string cwd = getCurrentWorkingDirectory();
   std::string command =
       "cd /d " + cwd +
@@ -93,12 +101,14 @@ void compile_dll() {
   system(command.c_str());
 }
 
-void copy_dll(const std::string &src, const std::string &dest) {
+void copy_dll(const std::string &src, const std::string &dest)
+{
   std::filesystem::copy_file(src, dest,
                              std::filesystem::copy_options::overwrite_existing);
 }
 
-void watch_src_directory() {
+void watch_src_directory()
+{
 
   std::cout << "inside watch_src_directory" << std::endl;
   HANDLE hDir = CreateFileA(
@@ -106,7 +116,8 @@ void watch_src_directory() {
       FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE, NULL,
       OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS | FILE_FLAG_OVERLAPPED, NULL);
 
-  if (hDir == INVALID_HANDLE_VALUE) {
+  if (hDir == INVALID_HANDLE_VALUE)
+  {
     std::cerr << "CreateFile failed (" << GetLastError() << ")" << std::endl;
     return;
   }
@@ -116,24 +127,28 @@ void watch_src_directory() {
   OVERLAPPED overlapped = {};
   overlapped.hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
 
-  if (overlapped.hEvent == NULL) {
+  if (overlapped.hEvent == NULL)
+  {
     std::cerr << "CreateEvent failed (" << GetLastError() << ")" << std::endl;
     CloseHandle(hDir);
     return;
   }
 
   std::cout << "handle is valid being watch loop" << std::endl;
-  while (true) {
+  while (true)
+  {
     if (ReadDirectoryChangesW(
             hDir, buffer, sizeof(buffer), TRUE,
             FILE_NOTIFY_CHANGE_FILE_NAME | FILE_NOTIFY_CHANGE_DIR_NAME |
                 FILE_NOTIFY_CHANGE_ATTRIBUTES | FILE_NOTIFY_CHANGE_SIZE |
                 FILE_NOTIFY_CHANGE_LAST_WRITE,
-            &bytesReturned, &overlapped, NULL)) {
+            &bytesReturned, &overlapped, NULL))
+    {
       WaitForSingleObject(overlapped.hEvent, INFINITE);
       FILE_NOTIFY_INFORMATION *pNotify;
       int offset = 0;
-      do {
+      do
+      {
         pNotify = (FILE_NOTIFY_INFORMATION *)((char *)buffer + offset);
         std::wstring fileName(pNotify->FileName,
                               pNotify->FileNameLength / sizeof(WCHAR));
@@ -149,16 +164,20 @@ void watch_src_directory() {
   }
 }
 
-void begin_watch_src_directory(game &g) {
+void begin_watch_src_directory(game &g)
+{
 
   std::cout << "calling watch_src_directory" << std::endl;
   std::thread watchThread(watch_src_directory);
   watchThread.detach();
 }
 
-void begin_game_loop(game &g) {
-  while (g.play) {
-    if (reloadFlag.load()) {
+void begin_game_loop(game &g)
+{
+  while (g.play)
+  {
+    if (reloadFlag.load())
+    {
       reloadFlag.store(false);
       printf("Reloading...\n");
       unloadlibrary(g.engine_lib);
--- dungeon1/src/engine/engine.cpp
+++ dungeon1/src/engine/engine.cpp
@@ -11,167 +11,192 @@
 
 #include "ecs.h"
 
-EXPORT void load_level(game *g, const char* sceneFilePath){
-  printf("load level at path %s\n", sceneFilePath);
+EXPORT void load_level(game *g, const char *sceneFilePath)
+{
+	printf("load level at path %s\n", sceneFilePath);
 
-  FILE *fp;
-    char errbuf[200];
+	FILE *fp;
+	char errbuf[200];
 
-    fp = fopen(sceneFilePath, "r");
-    if (!fp) {
-        printf("Cannot open %s - %s\n", sceneFilePath, strerror(errno));
-        return;
-    }
+	fp = fopen(sceneFilePath, "r");
+	if (!fp)
+	{
+		printf("Cannot open %s - %s\n", sceneFilePath, strerror(errno));
+		return;
+	}
 
-    toml_table_t *level = toml_parse_file(fp, errbuf, sizeof(errbuf));
-    fclose(fp);
+	toml_table_t *level = toml_parse_file(fp, errbuf, sizeof(errbuf));
+	fclose(fp);
 
-    printf("toml level loaded\n");
+	printf("toml level loaded\n");
+	World *w = get_world(g);
+	MemoryHeader *h = get_header(g);
 
-    for (int i = 0; ; i++) {
-        const char *friendly_name = toml_key_in(level, i);
-        if (!friendly_name) break;
+	for (int i = 0;; i++)
+	{
+		const char *friendly_name = toml_key_in(level, i);
+		if (!friendly_name)
+			break;
 
-        toml_table_t *attributes = toml_table_in(level, friendly_name);
-        if (attributes) {
-            printf("[%s]\n", friendly_name);
+		toml_table_t *attributes = toml_table_in(level, friendly_name);
+		if (attributes)
+		{
+			printf("[%s]\n", friendly_name);
+			size_t entity = create_entity(w);
+			for (int j = 0;; j++)
+			{
+				const char *type_key = toml_key_in(attributes, j);
+				if (!type_key)
+					break;
 
-            for (int j = 0; ; j++) {
-                const char *type_key = toml_key_in(attributes, j);
-                if (!type_key) break;
+				switch (mapStringToSubkeyType(type_key))
+				{
+				case POSITION_TYPE:
+				case SCALE_TYPE:
+				{
+					toml_table_t *nested_table = toml_table_in(attributes, type_key);
+					if (nested_table)
+					{
+						toml_datum_t x = toml_double_in(nested_table, "x");
+						toml_datum_t y = toml_double_in(nested_table, "y");
+						toml_datum_t z = toml_double_in(nested_table, "z");
 
-                 switch (mapStringToSubkeyType(type_key)) {
-                    case POSITION_TYPE: case SCALE_TYPE:  {
-                        toml_table_t *nested_table = toml_table_in(attributes, type_key);
-                        if (nested_table) {
-                            toml_datum_t x = toml_double_in(nested_table, "x");
-                            toml_datum_t y = toml_double_in(nested_table, "y");
-                            toml_datum_t z = toml_double_in(nested_table, "z");
+						printf("  %s = { x = %.2f, y = %.2f, z = %.2f }\n", type_key, x.u.d, y.u.d, z.u.d);
+					}
+					break;
+				}
+				case ROTATION_TYPE:
+				{
+					toml_table_t *nested_table = toml_table_in(attributes, type_key);
+					if (nested_table)
+					{
+						toml_datum_t x = toml_double_in(nested_table, "pitch");
+						toml_datum_t y = toml_double_in(nested_table, "yaw");
+						toml_datum_t z = toml_double_in(nested_table, "roll");
 
-                            printf("  %s = { x = %.2f, y = %.2f, z = %.2f }\n", type_key, x.u.d, y.u.d, z.u.d);
-                        }
-                        break;
-                    }
-                    case ROTATION_TYPE:{
-                        toml_table_t *nested_table = toml_table_in(attributes, type_key);
-                        if (nested_table) {
-                            toml_datum_t x = toml_double_in(nested_table, "pitch");
-                            toml_datum_t y = toml_double_in(nested_table, "yaw");
-                            toml_datum_t z = toml_double_in(nested_table, "roll");
+						printf("  %s = { x = %.2f, y = %.2f, z = %.2f }\n", type_key, x.u.d, y.u.d, z.u.d);
+					}
+					break;
+				}
+				case COLOR_TYPE:
+				{
+					toml_table_t *nested_table = toml_table_in(attributes, type_key);
+					if (nested_table)
+					{
+						toml_datum_t r = toml_double_in(nested_table, "r");
+						toml_datum_t g = toml_double_in(nested_table, "g");
+						toml_datum_t b = toml_double_in(nested_table, "b");
 
-                            printf("  %s = { x = %.2f, y = %.2f, z = %.2f }\n", type_key, x.u.d, y.u.d, z.u.d);
-                        }
-                        break;
-                    }
-                    case COLOR_TYPE: {
-                        toml_table_t *nested_table = toml_table_in(attributes, type_key);
-                        if (nested_table) {
-                            toml_datum_t r = toml_double_in(nested_table, "r");
-                            toml_datum_t g = toml_double_in(nested_table, "g");
-                            toml_datum_t b = toml_double_in(nested_table, "b");
+						printf("  %s = { r = %.2f, g = %.2f, b = %.2f }\n", type_key, r.u.d, g.u.d, b.u.d);
+					}
+					break;
+				}
+				case MODEL_TYPE:
+				{
+					break;
+				}
+				case MATERIAL_TYPE:
+				{
+					break;
+				}
+				case TEXTURE_TYPE:
+				{
+					break;
+				}
+				case FOV_TYPE:
+				{
+					toml_datum_t fov_value = toml_double_in(attributes, type_key);
+					if (fov_value.ok)
+						printf("  %s = %.2f\n", type_key, fov_value.u.d);
+					break;
+				}
+				case INTENSITY_TYPE:
+				{
+					toml_datum_t intensity_value = toml_double_in(attributes, type_key);
+					if (intensity_value.ok)
+						printf("  %s = %.2f\n", type_key, intensity_value.u.d);
+					break;
+				}
+				default:
+					printf("  %s = (unknown type)\n", type_key);
+				}
+			}
+			printf("\n");
+		}
+	}
 
-                            printf("  %s = { r = %.2f, g = %.2f, b = %.2f }\n", type_key, r.u.d, g.u.d, b.u.d);
-                        }
-                        break;
-                    }
-                    case MODEL_TYPE: {
-                        break;
-                    }
-                    case MATERIAL_TYPE: {
-                        break;
-                    }
-                    case TEXTURE_TYPE: {
-                        break;
-                    }
-                    case FOV_TYPE: {
-                        toml_datum_t fov_value = toml_double_in(attributes, type_key);
-                        if (fov_value.ok) printf("  %s = %.2f\n", type_key, fov_value.u.d);
-                        break;
-                    }
-                    case INTENSITY_TYPE: {
-                        toml_datum_t intensity_value = toml_double_in(attributes, type_key);
-                        if (intensity_value.ok) printf("  %s = %.2f\n", type_key, intensity_value.u.d);
-                        break;
-                    }
-                    default:
-                        printf("  %s = (unknown type)\n", type_key);
-                }
-            }
-            printf("\n");
-        }
-    }
-
-    toml_free(level);
+	toml_free(level);
 }
 
 EXPORT void init_engine(game *g)
 {
-  init_engine_memory(g);
+	init_engine_memory(g);
 }
 
 EXPORT void hotreloadable_imgui_draw(game *g)
 {
-  ImGui::SetCurrentContext(g->ctx);
-  ImGui::SetAllocatorFunctions(g->alloc_func, g->free_func, g->user_data);
-  ImGui::Begin("Hello! world from reloadable dll!");
-  ImGui::Text("This is some useful text.");
+	ImGui::SetCurrentContext(g->ctx);
+	ImGui::SetAllocatorFunctions(g->alloc_func, g->free_func, g->user_data);
+	ImGui::Begin("Hello! world from reloadable dll!");
+	ImGui::Text("This is some useful text.");
 
-  World *w = get_world(g);
-  MemoryHeader *h = get_header(g);
-  size_t count = w->entity_count;
+	World *w = get_world(g);
+	MemoryHeader *h = get_header(g);
+	size_t count = w->entity_count;
 
-  static size_t selected_entity = SIZE_MAX;
+	static size_t selected_entity = SIZE_MAX;
 
-  if (ImGui::Button("add entity"))
-  {
-    selected_entity = create_entity(w);
-  }
+	if (ImGui::Button("add entity"))
+	{
+		selected_entity = create_entity(w);
+	}
 
-  ImGui::Text("Entity List:");
-  for (size_t i = 0; i < count; ++i)
-  {
-    if (ImGui::Selectable(w->entity_names[i], selected_entity == i))
-    {
-      selected_entity = i;
-    }
-  }
+	ImGui::Text("Entity List:");
+	for (size_t i = 0; i < count; ++i)
+	{
+		if (ImGui::Selectable(w->entity_names[i], selected_entity == i))
+		{
+			selected_entity = i;
+		}
+	}
 
-  ImGui::Separator();
+	ImGui::Separator();
 
-  if (selected_entity != SIZE_MAX)
-  {
-    ImGui::Text("Inspecting Entity: %s", w->entity_names[selected_entity]);
+	if (selected_entity != SIZE_MAX)
+	{
+		ImGui::Text("Inspecting Entity: %s", w->entity_names[selected_entity]);
 
-    static int selected_component = -1;
-    
-    if (ImGui::BeginCombo("Add Component", "Select Component"))
-    {
-      for (int i = 0; i < component_count; ++i)
-      {
-        if (ImGui::Selectable(component_names[i], selected_component == i))
-        {
-          selected_component = i;
-          add_component(h, selected_entity, (1 << i));
-        }
-      }
-      ImGui::EndCombo();
-    }
-    
-    ImGui::Separator();
-    if(w->component_masks[selected_entity]){
-    ImGui::Text("Components:");
-    }
-    if (w->component_masks[selected_entity] & COMPONENT_POSITION)
-    {
-      Vec3 position;
-      
-      get_component_value(h, selected_entity, COMPONENT_POSITION, &position);
-      if (ImGui::InputFloat3("Position", (float *)&position))
-      {
-        set_component_value(h, selected_entity, COMPONENT_POSITION, position);
-      }
-    }
-  }
+		static int selected_component = -1;
 
-  ImGui::End();
+		if (ImGui::BeginCombo("Add Component", "Select Component"))
+		{
+			for (int i = 0; i < component_count; ++i)
+			{
+				if (ImGui::Selectable(component_names[i], selected_component == i))
+				{
+					selected_component = i;
+					add_component(h, selected_entity, (1 << i));
+				}
+			}
+			ImGui::EndCombo();
+		}
+
+		ImGui::Separator();
+		if (w->component_masks[selected_entity])
+		{
+			ImGui::Text("Components:");
+		}
+		if (w->component_masks[selected_entity] & COMPONENT_POSITION)
+		{
+			Vec3 position;
+
+			get_component_value(h, selected_entity, COMPONENT_POSITION, &position);
+			if (ImGui::InputFloat3("Position", (float *)&position))
+			{
+				set_component_value(h, selected_entity, COMPONENT_POSITION, position);
+			}
+		}
+	}
+
+	ImGui::End();
 }
