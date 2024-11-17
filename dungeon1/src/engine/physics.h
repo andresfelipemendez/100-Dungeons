6b5f3b53b3e46728e5385a4e84a2cc9a9678bf09
Andrés Felipe Méndez <fmendez1212@gmail.com>
2024-11-17 18:54:00 -0500

    getting started on the physics engine

--- dungeon1/src/engine/physics.h
+++ dungeon1/src/engine/physics.h
@@ -1,11 +1,36 @@
 #ifndef PHYSICS_H
 #define PHYSICS_H
 
-struct nv3 {
+#include <cmath>
+
+struct hv3 {
 	float x;
 	float y;
 	float z;
-	float n;
+	float w;
+
+	hv3 &operator*=(float scalar) {
+		x *= scalar;
+		y *= scalar;
+		z *= scalar;
+		return *this;
+	}
+
+	inline float magnitude() const { return std::sqrt(x * x + y * y + z * z); }
+
+	inline float squareMagnitude() const { return x * x + y * y + z * z; }
+
+	inline void normalize() {
+		float l = magnitude();
+		if (l > 0) {
+			*this *= (1.f / l);
+		}
+	}
+
+	hv3 operator%(const hv3 &other) const {
+		return hv3{y * other.z - z * other.y, z * other.x - x * other.z,
+				   x * other.y - y * other.x, 0.0f};
+	}
 };
 
 #endif // PHYSICS_H
