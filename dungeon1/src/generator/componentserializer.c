f4e6b9c8b376a6b511e176ad90a5026dddbed8c6
Andrés Felipe Méndez <fmendez1212@gmail.com>
2024-11-20 20:38:03 -0500

    starting to parse the header

--- dungeon1/src/generator/componentserializer.c
+++ dungeon1/src/generator/componentserializer.c
@@ -1,28 +1,74 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-    FILE* componentsFile = NULL;
-    fopen_s(&componentsFile, "..\\..\\src\\generator\\components.h", "r");
-    if(!componentsFile) {
-        fopen_s(&componentsFile, "src\\generator\\components.h", "r");
-    }
 
-    if(!componentsFile) {
-        perror("couldn't find the component.h file\n");
-        return EXIT_FAILURE;
-    }
-    
-    int c;
-    while((c=fgetc(componentsFile))!=EOF){
-        putchar(c);
-    }
-    
-    if(ferror(componentsFile)){
-        puts("I/O error when reading");
-    }else if(feof(componentsFile)){
-        puts("End of file is reached succesfully");
-    }
-    fclose(componentsFile);
-    return EXIT_SUCCESS;
+enum parsingState {
+	start,
+	structKeyword,
+	componentName,
+	startFields,
+	filedType,
+	fieldName,
+	endFiledDefinition,
+	endComponentDefinition,
+};
+
+int main() {
+	FILE *componentsFile = NULL;
+	fopen_s(&componentsFile, "..\\..\\src\\generator\\components.h", "r");
+	if (!componentsFile) {
+		fopen_s(&componentsFile, "src\\generator\\components.h", "r");
+	}
+
+	if (!componentsFile) {
+		perror("couldn't find the component.h file\n");
+		return EXIT_FAILURE;
+	}
+	enum parsingState state = structKeyword;
+	int c;
+	while ((c = fgetc(componentsFile)) != EOF) {
+		switch (state) {
+		case start:
+			break;
+		case structKeyword: {
+			char structKeyword[] = "struct";
+			static int i = 0;
+			if (c == structKeyword[i]) {
+                ++i;
+			} else if (c ==' ' && i == 6) {
+                puts("finished the struct keyword, component name is:");
+                i = 0;
+                state = componentName;
+            }
+			break;
+		}
+		case componentName:
+        
+		    putchar(c);
+            if(c == ' ') {
+                putchar('\n');
+                state = startFields;
+            }
+			break;
+		case startFields:
+        puts("the fields are:");
+			break;
+		case filedType:
+			break;
+		case fieldName:
+			break;
+		case endFiledDefinition:
+			break;
+		case endComponentDefinition:
+			break;
+		}
+	}
+
+	if (ferror(componentsFile)) {
+		puts("I/O error when reading");
+	} else if (feof(componentsFile)) {
+		puts("End of file is reached succesfully");
+	}
+	fclose(componentsFile);
+	return EXIT_SUCCESS;
 }
