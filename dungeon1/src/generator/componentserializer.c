d4432cc69f2ad880f092124c3fc2ef47b6ddb798
Andrés Felipe Méndez <fmendez1212@gmail.com>
2024-11-20 20:09:45 -0500

    open and read the file

--- dungeon1/src/generator/componentserializer.c
+++ dungeon1/src/generator/componentserializer.c
@@ -1,5 +1,28 @@
+#include <stdlib.h>
 #include <stdio.h>
 
 int main(){
-    printf("hello\n");
+    FILE* componentsFile = NULL;
+    fopen_s(&componentsFile, "..\\..\\src\\generator\\components.h", "r");
+    if(!componentsFile) {
+        fopen_s(&componentsFile, "src\\generator\\components.h", "r");
+    }
+
+    if(!componentsFile) {
+        perror("couldn't find the component.h file\n");
+        return EXIT_FAILURE;
+    }
+    
+    int c;
+    while((c=fgetc(componentsFile))!=EOF){
+        putchar(c);
+    }
+    
+    if(ferror(componentsFile)){
+        puts("I/O error when reading");
+    }else if(feof(componentsFile)){
+        puts("End of file is reached succesfully");
+    }
+    fclose(componentsFile);
+    return EXIT_SUCCESS;
 }
